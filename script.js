116e6661427fb1e7b2e3c533028d75b564a5d83d
Enemirovskaya <elenanemirovskaya1984@gmail.com>
2022-10-12 17:07:38 -0400

    demo version

---
+++ script.js
@@ -0,0 +1,26 @@
+var questionOne = ['How do you write "Hello world" in an alert-box?'];
+var answerOne = ['a. alertBox("Hello World")', 'b. alert("Hello World")', 'c. msg("Hello World")', 'd. msgBox("Hello World")'];
+
+var questionTwo = ['How to write an IF statment?'];
+var answerTwo = ['a. if i=5 then', 'b. if i==5 then', 'c. if (i==5)', 'd. if i=5'];
+
+var questionThree = ['Inside which HTML element do we put the Java Script?'];
+var answerTwo = ['a. <Js>', 'b. <scripting>', 'c. <script>', 'd. <javascript>'];
+
+var questionFour = ['Where is correct place to insert a JavaScript?'];
+var answerFour = ['a. <head> & <body>','b. the <body> section','c. the <head> section','d. in <footer>'];
+
+var questionFive = ['Which of the following is an example of an array?'];
+var answerFive = ['a. var = array()', 'b. var = array{}', 'c. var = array[]', 'd. var = array<>'];
+
+var allQuestions = [questionOne, questionTwo, questionThree, questionFour, questionFive];
+
+const start_quiz_btn = document.querySelector('.bottom-box');
+const submit_btn = document.querySelector('.questions');
+const go_back_btn = document.querySelector('.finish-quiz');
+const clear_scores_btn = document.querySelector('.show-highscores');
+
+start_quiz_btn.addEventListener('click', function(){submit_btn.display} )
+function(questionsSection){
+
+}
